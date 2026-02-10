cc00c5bf300dfcdc0d3b71f4388962b10f4b5f32
Juan Davel <juan@ringwoodgroup.co.za>
2026-02-10 14:16:54 +0200

    IniFileSettings::SetPassword ETBlowFish -> ETSHA256.

--- altimailserver/source/Server/Common/Application/IniFileSettings.cpp
+++ altimailserver/source/Server/Common/Application/IniFileSettings.cpp
@@ -479,8 +479,11 @@ namespace HM
    {
       password_ = sNewVal;
 
-      WriteIniSetting_("Database", "Password", Crypt::Instance()->EnCrypt(password_, Crypt::ETBlowFish));
-      WriteIniSetting_("Database", "PasswordEncryption", Crypt::ETBlowFish);
+      //WriteIniSetting_("Database", "Password", Crypt::Instance()->EnCrypt(password_, Crypt::ETBlowFish));
+      //WriteIniSetting_("Database", "PasswordEncryption", Crypt::ETBlowFish);
+
+      WriteIniSetting_("Database", "Password", Crypt::Instance()->EnCrypt(password_, Crypt::ETSHA256));
+      WriteIniSetting_("Database", "PasswordEncryption", Crypt::ETSHA256);
    }
 
    void 
