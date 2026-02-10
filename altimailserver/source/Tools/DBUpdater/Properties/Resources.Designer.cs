90409d31910214711d2207b8d627b41c98d907d3
Juan Davel <juan@ringwoodgroup.co.za>
2026-02-10 13:38:46 +0200

    High-DPI (preliminary) support. Resources for main form.

--- altimailserver/source/Tools/DBUpdater/Properties/Resources.Designer.cs
+++ altimailserver/source/Tools/DBUpdater/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace DBUpdater.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap db_warning {
+            get {
+                object obj = ResourceManager.GetObject("db-warning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
